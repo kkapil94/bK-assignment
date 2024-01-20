73693e77ad52a87c3c64bbb758cf69ef86d8fcec
kapil <khatrikapil987@gmail.com>
2024-01-20 11:16:43 +0530

    backend updated

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
-const connect = () => {
-    return mongoose.connect(`mongodb+srv://kapil:${process.env.MONGO_PASSWORD}@cluster0.gnmjov6.mongodb.net/?retryWrites=true&w=majority`);
-  };
-  
-  const data = (filterQuery) => {
-    const dataCollection = mongoose.connection.useDb("blackCoffer").collection("data");
-    return dataCollection.find(filterQuery);
-  };
-  
-  export {connect,data}
+const connect = async() => {
+  const conn = await mongoose.connect(
+    `mongodb+srv://kapil:${process.env.MONGO_PASSWORD}@cluster0.gnmjov6.mongodb.net/?retryWrites=true&w=majority`
+  );
+  return conn;
+};
+
+export default connect;
